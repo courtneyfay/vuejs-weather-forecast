64c21556b2946ebb937869ada19b627044a6eeff
Courtney Fay <fay.courtney@gmail.com>
2019-06-11 19:17:36 -0600

    initial commit. adding header, navigation, routing and draft page views

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/">VueJS Weather</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <router-link to="/">
+              <i class="fa fa-home"></i>
+              Home
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/forecast">Forecast</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
---
+++ src/router.js
@@ -0,0 +1,24 @@
+//Utilities
+import Vue from "vue";
+import Router from "vue-router";
+
+//Views
+import HomePage from "./views/HomePage.vue";
+import ForecastPage from "./views/ForecastPage.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: HomePage
+    },
+    {
+      path: "/forecast",
+      name: "Forecast",
+      component: ForecastPage
+    }
+  ]
+});
